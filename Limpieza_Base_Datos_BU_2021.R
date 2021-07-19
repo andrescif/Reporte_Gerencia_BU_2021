c696f61dd1526c1b57035b8c4c23f3db36379958
Andres Cifuentes <andrescif@gmail.com>
2021-07-19 11:37:50 -0600

    Inicio del limpiado de la base de datos.

---
+++ Limpieza_Base_Datos_BU_2021.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(eeptools)
+library(readr)
+#Cargar datos
+BASE_DATOS_PSOCIALES_JULIO2021 <- read_csv("Datos/BASE DATOS PSOCIALES B U JULIO2021.csv", 
+                                +   col_names = FALSE, col_types = cols(CUI = col_double()))
+
+
+
+
+
+
+
+
+
+
+
+
+
